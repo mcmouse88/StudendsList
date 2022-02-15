91446db88c15c9210fb842c80e1ddc1f382db259
sergey-r88 <mcmouse88@icloud.com>
2022-02-15 19:59:45 +0200

    chanched HashMap on HashSet

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -13,6 +13,10 @@ public class Student {
         this.studentId = studentId;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
